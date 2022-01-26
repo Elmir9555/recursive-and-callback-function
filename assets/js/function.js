d03ad0eddc437885392885b9b83a3cf185be47ff
Elmir9555 <elmiriu@code.edu.az>
2022-01-26 23:35:45 +0400

    callback and recursive function

---
+++ assets/js/function.js
@@ -0,0 +1,43 @@
+// let array=[1,2,3,4,5,6,7,8,9]
+
+// function sumOdd(arr,callback) {
+    
+//     let sum=0;
+//     for (const item of arr) {
+//         if (callback(item)) {
+//             sum +=item
+//         }
+//     }
+//     console.log(sum);
+// }
+
+// function isOdd(a) {
+//     return a%2==1
+// }
+
+// sumOdd(array,isOdd)
+
+
+// let arr=[1,2,3,4,5,6]
+
+// // let newArr=arr.map(a => a * 2);
+
+// // console.log(newArr);
+
+// function  createArray(array,n) {
+//     let newArr=[];
+//     for (const item of array) {
+//         newArr.push(item*n)
+//     }
+//     return newArr;
+// }
+
+// console.log(createArray(arr,3)); ;
+
+
+// let arr=[1,2,3,4,5,6,7,8]
+
+// arr.forEach(n => {
+//     console.log(n);
+// });
+
